a9a45c11db60099483982ec1da9191aae6dca865
Carolineliu <fruitsugar1@outlook.com>
2015-08-17 15:54:34 +0800

    finish post and delete

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 var hbs = require('hbs');
+var bodyParser = require('body-parser');
 var result = require('./writeAllScores.js');
 app.use(express.static('node_modules'));
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 var mysql = require('mysql');
@@ -16,12 +20,13 @@ var pool = mysql.createPool({
   database: 'studentscore',
 });
 var conn;
-app.get('*', function(req, res, next) {
+app.all('*', function(req, res, next) {
   pool.getConnection(function(err, connection) {
     conn = connection;
     next();
   });
 });
+
 
 app.get('/', function(req, res, next) {
   conn.query({
@@ -49,13 +54,13 @@ app.get("/sort", function(req, res) {
       return (parseInt(a[sortKey]) - parseInt(b[sortKey])) * asc;
     });
     res.send(array);
-   conn.release();
+    conn.release();
   });
 });
 
 app.delete('/delete', function(req, res) {
-  var sortkey=req.query.sortKey;
-  var sortKey=parseInt(sortkey);
+  var sortkey = req.query.sortKey;
+  var sortKey = parseInt(sortkey);
   var deleteSQL = 'delete from scores where student_id =' + sortKey;
   conn.query({
     sql: deleteSQL
@@ -66,22 +71,48 @@ app.delete('/delete', function(req, res) {
   });
 });
 
-// app.get("/detele", function(req, res) {
-//   var sortKey = req.query.sortKey;
-//   var deleteSQL = 'delete from scores where student_id =' + sortKey;
-//   var getSQL = ' select scores.scores_id,student.student_id,student.student_name,course.course_name,scores.scores from student,scores,course  where student.student_id = scores.student_id and course.course_id = scores.course_id';
-//   connection.query({
-//     sql: deleteSQL
-//   }, function(error, results) {
-//     // var newarray = result(results);
-//     console.log(newarray);
-//     console.log(sortKey);
-//     if (error) throw error;
-//
-//     res.send(1);
-//   });
+app.post('/post', function(req, res) {
+  // var id = parseInt(req.body.student_id);
+  var name = req.body.student_name;
+  var chinese = parseInt(req.body.chinese);
+  var math = parseInt(req.body.math);
+  var insertStudent = 'insert into student(student_name) values(\'' + name + '\');';
+  conn.query(insertStudent, function(error, results) {
+    if (error) throw error;
+    var id = results.insertId;
+    var insertScores1 = 'insert into scores(student_id,course_id,scores) values(' + '\'' +
+      id + '\'' + ',' + 1 + ',' + chinese + ')';
+    conn.query(insertScores1, function(error, results) {
+      if (error) throw error;
+      var insertScores2 = 'insert into scores(student_id,course_id,scores) values(' + '\'' +
+        id + '\'' + ',' + 2 + ',' + math + ')';
+      conn.query(insertScores2, function(error, results) {
+        if (error) throw error;
+        var temp={id:id};
+        res.send(temp);
+      });
+      // conn.release();
+    });
+  });
+});
+
+// app.put('/put', function(req, res) {
+//   var id = parseInt(req.query.student_id);
+//   var name = req.query.student_name;
+//   var chinese = parseInt(req.body.chinese);
+//   var math = parseInt(req.body.math);
+//   console.log(name);
+//   console.log(chinese);
+//   // var updateSQL = "update name set birthday='1971-01-10' where uname='张三';
+//   // conn.query({
+//   //   sql: updateSQL
+//   // }, function(error, results) {
+//   //   if (error) throw error;
+//   //   res.send("true");
+//   //   // conn.release();
+//   // });
 // });
-//
+
 var server = app.listen(5000, function() {
   console.log("app listen ");
 });
--- public/main.js
+++ public/main.js
@@ -14,7 +14,6 @@ $(function() {
           "<td>" + val.id + "</td>" +
           "<td>" + val.name + "</td>" +
           "<td>" + val.chinese + "</td>" + "<td>" + val.math + "</td>" +
-          // "<td>" + "增加 " + "</td>" +
           "<td>" + "删除 " + "</td>" +
           "</tr>");
       });
@@ -24,61 +23,74 @@ $(function() {
 
   $("#world").on("click", "tr", function() {
     var sortKey = $(this).data('name');
-    var tr=$(this);
+    var r=confirm("delete this rows,Are you sure?");
+    var tr = $(this);
     $.ajax({
-      url: '/delete?sortKey='+sortKey,
-      // "/admin/list?id=" +id'',
+      url: '/delete?sortKey=' + sortKey,
       type: 'DELETE',
-      data:'sortKey',
-      success:function(data) {
+      success: function(data) {
         if (data === "true") {
-          console.log(tr);
           $(tr).remove();
         }
       }
     });
-    // $.delete('/delete', {
-    //   sortKey: sortKey
-    // }, function(reply) {
-    //
-    //   console.log(sortKey);
-    //   $("#world").empty();
-    //   console.log(reply);
-    //   reply.forEach(function(val) {
-    //
-    //     $("#world").append("<tr>" +
-    //       "<td>" + val.id + "</td>" +
-    //       "<td>" + val.name + "</td>" +
-    //       "<td>" + val.chinese + "</td>" + "<td>" + val.math + "</td>" +
-    //       "<td>" + "增加 " + "</td>" +
-    //       "<td>" + "删除 " + "</td>" +
-    //       "</tr>");
-    //   });
-    // });
-
   });
+});
 
-  // $("#world").on("click", "tr", function() {
-  //   var sortKey = $(this).data('name');
-  //   $.delete('/delete', {
-  //     sortKey: sortKey
-  //   }, function(reply) {
-  //
-  //     console.log(sortKey);
-  //     $("#world").empty();
-  //     console.log(reply);
-  //     reply.forEach(function(val) {
-  //
-  //       $("#world").append("<tr>" +
-  //         "<td>" + val.id + "</td>" +
-  //         "<td>" + val.name + "</td>" +
-  //         "<td>" + val.chinese + "</td>" + "<td>" + val.math + "</td>" +
-  //         "<td>" + "增加 " + "</td>" +
-  //         "<td>" + "删除 " + "</td>" +
-  //         "</tr>");
-  //     });
-  //   });
-  // });
+$(function() {
+  $("#add1").on("click", function() {
+    var name = $("#name").val();
+    var chinese = $("#chinese").val();
+    var math = $("#math").val();
+    $.post('/post', {
+      student_name: name,
+      chinese: chinese,
+      math: math
+    }, function(id) {
+       var realId=id.id;
+      console.log(id);
+      if (realId > 0) {
+        console.log(1);
+        $("#world").append("<tr>" +
+          "<td>" + realId + "</td>" +
+          "<td>" + name + "</td>" +
+          "<td>" + chinese + "</td>" + "<td>" + math + "</td>" +
+          "<td>" + "删除 " + "</td>" +
+          "</tr>");
+      }
+      console.log(1);
+    });
+  });
+});
 
 
+$(function() {
+  $("#put1").on("click", function() {
+    alert(3);
+    var id = $("#Pid").val();
+    var name = $("#Pname").val();
+    var chinese = $("#Pchinese").val();
+    var math = $("#Pmath").val();
+    var data = {
+      id: id,
+      name: name,
+      chinese: chinese,
+      math: math
+    };
+    $.ajax({
+      url: '/put',
+      type: 'PUT',
+      data: data,
+      success: function(data) {
+        if (data === "true") {
+          $("#world tr").html("<tr>" +
+            "<td>" + id + "</td>" +
+            "<td>" + name + "</td>" +
+            "<td>" + chinese + "</td>" + "<td>" + math + "</td>" +
+            "<td>" + "删除 " + "</td>" +
+            "</tr>");
+        }
+      }
+    });
+  });
 });
